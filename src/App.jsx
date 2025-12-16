82d9ef9e007cc2b71361e638cc9091806580d4ad
happiness olukayode <olukayodehappiness2006@gmail.com>
2025-12-16 08:35:14 +0100

    added filter funct.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
 import JobComponent from './JobComponent'
+import dataJson from '../data.json'
+import removeIcon from './assets/icon-remove.svg'
 const App = () => {
+  const [data, setData] = useState(dataJson)
+  const [activeFilters, setActiveFilters] = useState([])
+
+  const removeFilter = tag => {
+    setActiveFilters(activeFilters.filter(f => f !== tag))
+  }
+
+  const clearAllFilters = () => {
+    setActiveFilters([])
+  }
+  const handletagClick = tag => {
+    if (!activeFilters.includes(tag)) {
+      return setActiveFilters([...activeFilters, tag])
+    }
+  }
+  const filteredData = data.filter(jobListing => {
+    if (activeFilters.length === 0) return true
+    return activeFilters.every(filter => {
+      return (
+        jobListing.role === filter ||
+        jobListing.level === filter ||
+        jobListing.languages.includes(filter) ||
+        jobListing.tools.includes(filter)
+      )
+    })
+  })
   return (
     <div className="bg-gr-50">
-      <div className="bg-gr-400  h-42 bg-[url('/bg-header-mobile.svg')] sm:bg-[url('/bg-header-desktop.svg')] bg-cover bg-center bg-no-repeat"></div>
-      <div className="max-w-300 px-5 mx-auto mt-17 flex flex-col items-center justify-center gap-10 min-[987px]:gap-5">
-        <JobComponent />
-        <JobComponent />
-        <JobComponent />
+      <div className="bg-gr-400  h-42 bg-[url('/bg-header-mobile.svg')] sm:bg-[url('/bg-header-desktop.svg')] bg-cover bg-center bg-no-repeat relative">
+        {activeFilters.length > 0 && (
+          <ul
+            className="max-w-300 absolute bottom-0 translate-y-1/2 translate-x-1/2
+ right-1/2 mx-auto bg-white w-[95%] shadow-xl shadow-gr-900/10 p-4 rounded-md  flex items-center justify-between gap-4 "
+          >
+            <div className="flex gap-2 flex-wrap">
+              {activeFilters.map((filter, i) => (
+                <li
+                  className="flex items-center justify-between gap-1 text-gr-400 rounded-sm bg-gr-400/30 font-semibold text-sm overflow-hidden"
+                  key={i}
+                >
+                  <span className="p-1 px-2">{filter}</span>
+
+                  <button
+                    className="bg-gr-400 p-2 shrink-0 cursor-pointer transition-all duration-300 hover:bg-gr-900"
+                    onClick={() => removeFilter(filter)}
+                  >
+                    <img src={removeIcon} alt="" />
+                  </button>
+                </li>
+              ))}
+            </div>
+            <div
+              className="text-gr-400 font-semibold transition-all duration-300 hover:underline hover:underline-offset-1 cursor-pointer"
+              onClick={clearAllFilters}
+            >
+              Clear
+            </div>
+          </ul>
+        )}
+      </div>
+
+      <div className="max-w-300 p-5 mx-auto mt-17 flex flex-col items-center justify-center gap-10 min-[987px]:gap-5">
+        {filteredData.map(jobListing => (
+          <JobComponent
+            key={jobListing.id}
+            logo={jobListing.logo}
+            company={jobListing.company}
+            position={jobListing.position}
+            postedAt={jobListing.postedAt}
+            contract={jobListing.contract}
+            location={jobListing.location}
+            entails={[
+              jobListing.role,
+              jobListing.level,
+              ...jobListing.languages,
+            ]}
+            isNew={jobListing.new}
+            isFeatured={jobListing.featured}
+            onTagClick={handletagClick}
+          />
+        ))}
       </div>
     </div>
   )
--- src/JobComponent.jsx
+++ src/JobComponent.jsx
@@ -1,41 +1,61 @@
 import React from 'react'
-import assets from './assets/assets'
-const JobComponent = () => {
+const JobComponent = ({
+  logo,
+  company,
+  position,
+  postedAt,
+  contract,
+  location,
+  entails,
+  isNew = false,
+  isFeatured = false,
+  onTagClick,
+}) => {
   return (
-    <article className="relative bg-white w-full shadow-xl shadow-gr-900/10 p-4 pt-10  min-[987px]:p-4 rounded-md flex justify-between gap-10 min-[987px]:gap-3 items-start min-[987px]:items-center border-l-6 border-gr-400 flex-col min-[987px]:flex-row">
+    <article
+      className={`relative bg-white w-full shadow-xl shadow-gr-900/10 p-4 pt-10  min-[987px]:p-4 rounded-md  flex justify-between gap-5 min-[987px]:gap-3 items-start min-[987px]:items-center ${
+        isFeatured ? 'border-l-6' : ''
+      } border-gr-400 flex-col min-[987px]:flex-row`}
+    >
       <div className="flex gap-3 items-center">
         <img
-          src={assets.photosnapIcon}
+          src={logo}
           alt=""
           className="max-[987px]:w-13 max-[987px]:h-13 max-[987px]:absolute max-[987px]:top-0 max-[987px]:-translate-y-1/2 max-[987px]:left-4 min-[987px]:relative"
         />
         <div className="flex flex-col gap-1">
           <div className="flex gap-3 items-center">
-            <span className="font-semibold text-gr-400 ">Photosnap</span>
-            <span className="inline-flex items-center justify-center bg-gr-400 text-white uppercase px-2 py-1 text-sm font-semibold rounded-full">
-              New!
-            </span>
-            <span className="inline-flex text-center items-center justify-center bg-gr-900 text-white uppercase px-2 py-1 text-sm font-semibold rounded-full">
-              featured
-            </span>
+            <span className="font-semibold text-gr-400 ">{company}</span>
+            {isNew && (
+              <span className="inline-flex items-center justify-center bg-gr-400 text-white uppercase px-2 py-1 text-sm font-semibold rounded-full">
+                New!
+              </span>
+            )}
+            {isFeatured && (
+              <span className="inline-flex text-center items-center justify-center bg-gr-900 text-white uppercase px-2 py-1 text-sm font-semibold rounded-full">
+                featured
+              </span>
+            )}
           </div>
           <h2 className="font-bold text-lg transition-all duration-300 hover:text-gr-400 cursor-pointer">
-            Senior Frontend Developer
+            {position}
           </h2>
           <div className="flex items-center gap-4 text-gra-400">
-            <span>5d ago</span>
+            <span>{postedAt}</span>
             <span className="w-1 h-1 bg-gra-400 rounded-full"></span>
-            <span>Contract</span>
+            <span>{contract}</span>
             <span className="w-1 h-1 bg-gra-400 rounded-full"></span>
-            <span>USA only</span>
+            <span>{location}</span>
           </div>
         </div>
       </div>
+      <div className="w-full h-px bg-gray-300 min-[987px]:hidden"></div>
       <div className="flex-wrap min-[987px]:flex-nowrap  flex gap-3">
-        {['Frontend', 'Senior', 'HTML', 'CSS', 'JavaScript'].map((item, i) => (
+        {entails.map((item, i) => (
           <span
             className="text-gr-400 bg-gr-400/10 cursor-pointer rounded-sm py-1 px-2 font-semibold transition-all duration-300 hover:text-white hover:bg-gr-400"
             key={i}
+            onClick={() => onTagClick(item)}
           >
             {item}
           </span>
